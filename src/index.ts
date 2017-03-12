41894c26f823245d52c7f9891823bf8a70e3a88b
Joseph Luck <joseph.luck@sky.com>
2017-03-12 16:10:06 +0000

    Remove existing types

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,3 @@
-export type Reducer = (state, ...args: any[]) => any
-export interface Reducers {
-  [key: string]: Reducer
-}
-export type Effect = (state, actions, ...args: any[]) => any
-export interface Effects {
-  [key: string]: Effect
-}
-export interface Model {
-  scoped?: boolean
-  state: any
-  reducers?: Reducers
-  effects?: Effects
-  models?: Models
-}
-export interface Models {
-  [key: string]: Model
-}
-export type Subscription = (state, prev, actions: Actions) => any
-export interface ConfigurationOpts {
-  onStateChange: Subscription
-  onMethodCall: any
-}
-export type Configuration = Subscription | ConfigurationOpts
-export interface Actions {
-  [key: string]: Reducer | Effect | Actions
-}
-export type State = any
-export interface Output {
-  state: any
-  actions: any
-}
-export type ReturnOutput = (model: Model) => Output
-
 function noop () {
   return null
 }
@@ -40,7 +6,7 @@ function arrayToObj (curr, prev) {
   return Object.assign({}, curr, prev)
 }
 
-export function merge (model: Model, prop: string) {
+export function merge (model, prop) {
   if (model.models) {
     let child = Object.keys(model.models).map(key => ({
       [key]: merge(model.models[key], prop),
@@ -51,11 +17,11 @@ export function merge (model: Model, prop: string) {
   return model[prop]
 }
 
-export function createState (model: Model): State {
+export function createState (model) {
   return merge(model, 'state')
 }
 
-export function retrieveNestedModel (model: Model, path: string[], index: number = 0): Model {
+export function retrieveNestedModel (model, path, index = 0) {
   if (model.models) {
     let currModel = model.models[path[index]]
     if (currModel && currModel.models && currModel.models[path[index + 1]]) {
@@ -66,7 +32,7 @@ export function retrieveNestedModel (model: Model, path: string[], index: number
   return model
 }
 
-export function getNestedObjFromPath (state, path: any[]) {
+export function getNestedObjFromPath (state, path) {
   if (path.length) {
     return getNestedObjFromPath(state[path[0]], path.slice(1))
   }
@@ -89,57 +55,51 @@ export function updateStateAtPath (obj, path, value) {
   return obj
 }
 
-export default function twine (opts?: Configuration): ReturnOutput {
+export default function twine (opts) {
   if (!opts) {
     opts = noop
   }
   let onStateChange = typeof opts === 'function' ? opts : opts.onStateChange || noop
   let onMethodCall = typeof opts === 'function' ? noop : opts.onMethodCall || noop
 
-  return function output (model: Model): Output {
+  return function output (model) {
     let state = createState(model)
     let actions = createActions(model, [])
 
-    function decorateActions (reducers: Reducers, effects: Effects, path: string[]): Actions {
-      const decoratedReducers = Object.keys(reducers || {}).map(key => {
-        return {
-          [key]: function () {
-            let oldState = Object.assign({}, state)
-            let localState = path.length ? getNestedObjFromPath(state, path) : state
-            let reducerArgs = [localState].concat(Array.prototype.slice.call(arguments))
-            let reducerResponse = reducers[key].apply(null, reducerArgs)
-            let newLocalState = Object.assign({}, localState, reducerResponse)
-            state = path.length ? updateStateAtPath(state, path, newLocalState) : newLocalState
-            let onMethodCallArgs = [state, oldState].concat(Array.prototype.slice.call(arguments))
-            onMethodCall.apply(null, onMethodCallArgs)
-            onStateChange(state, oldState, actions)
-            return newLocalState
-          },
-        }
-      })
-      const decoratedEffects = Object.keys(effects || {}).map(key => {
-        return {
-          [key]: function () {
-            if (path.length) {
-              let nestedModel = retrieveNestedModel(model, path)
-              let effectState = nestedModel.scoped ? getNestedObjFromPath(state, path) : state
-              let effectActions = nestedModel.scoped ? getNestedObjFromPath(actions, path) : actions
-              return effects[key].apply(null, [effectState, effectActions].concat(Array.prototype.slice.call(arguments)))
-            }
-            return effects[key].apply(null, [state, actions].concat(Array.prototype.slice.call(arguments)))
-          },
-        }
-      })
+    function decorateActions (reducers, effects, path) {
+      const decoratedReducers = Object.keys(reducers || {}).map(key => ({
+        [key]: function () {
+          let oldState = Object.assign({}, state)
+          let localState = path.length ? getNestedObjFromPath(state, path) : state
+          let reducerArgs = [localState].concat(Array.prototype.slice.call(arguments))
+          let reducerResponse = reducers[key].apply(null, reducerArgs)
+          let newLocalState = Object.assign({}, localState, reducerResponse)
+          state = path.length ? updateStateAtPath(state, path, newLocalState) : newLocalState
+          let onMethodCallArgs = [state, oldState].concat(Array.prototype.slice.call(arguments))
+          onMethodCall.apply(null, onMethodCallArgs)
+          onStateChange(state, oldState, actions)
+          return newLocalState
+        },
+      }))
+      const decoratedEffects = Object.keys(effects || {}).map(key => ({
+        [key]: function () {
+          if (path.length) {
+            let nestedModel = retrieveNestedModel(model, path)
+            let effectState = nestedModel.scoped ? getNestedObjFromPath(state, path) : state
+            let effectActions = nestedModel.scoped ? getNestedObjFromPath(actions, path) : actions
+            return effects[key].apply(null, [effectState, effectActions].concat(Array.prototype.slice.call(arguments)))
+          }
+          return effects[key].apply(null, [state, actions].concat(Array.prototype.slice.call(arguments)))
+        },
+      }))
       return decoratedReducers.concat(decoratedEffects).reduce(arrayToObj, {})
     }
 
-    function createActions (model: Model, path: string[]): Actions {
+    function createActions (model, path) {
       if (model.models) {
-        const child = Object.keys(model.models).map(key => {
-          return {
-            [key]: createActions(model.models[key], path.concat(key)),
-          }
-        }).reduce(arrayToObj, {})
+        const child = Object.keys(model.models).map(key => ({
+          [key]: createActions(model.models[key], path.concat(key)),
+        })).reduce(arrayToObj, {})
         return Object.assign({}, decorateActions(model.reducers, model.effects, path), child)
       }
       return decorateActions(model.reducers, model.effects, path)
