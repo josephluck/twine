e7eae3590c4096569d82e184df40cd93fb5d0d02
Joseph Luck <joseph.luck@sky.com>
2017-11-27 13:10:14 +0000

    Better names for type generics

--- src/types.ts
+++ src/types.ts
@@ -1,81 +1,81 @@
 export declare namespace Twine {
-  export type Subscriber<S, A> = (state: S, prev: S, actions: A) => any
-  export type OnReducerCalled<S> = (state: S, prev: S, name: string, ...args: any[]) => any
-  export type OnEffectCalled<S> = (prev: S, name: string, ...args: any[]) => any
+  export type Subscriber<State, Actions> = (state: State, prev: State, actions: Actions) => any
+  export type OnReducerCalled<State> = (state: State, prev: State, name: string, ...args: any[]) => any
+  export type OnEffectCalled<State> = (prev: State, name: string, ...args: any[]) => any
 
-  export type Plugin<S, A> =
-    | Subscriber<S, A>
+  export type Plugin<State, Actions> =
+    | Subscriber<State, Actions>
     | {
-        onStateChange?: Subscriber<S, A>
-        onReducerCalled?: OnReducerCalled<S>
-        onEffectCalled?: OnEffectCalled<S>
-        wrapReducers?: (reducer: ReducerApi<any, any>) => ReducerApi<any, any>
-        wrapEffects?: (effect: EffectApi<any, any>) => EffectApi<any, any>
-      }
+      onStateChange?: Subscriber<State, Actions>
+      onReducerCalled?: OnReducerCalled<State>
+      onEffectCalled?: OnEffectCalled<State>
+      wrapReducers?: (reducer: ReducerApi<any, any>) => ReducerApi<any, any>
+      wrapEffects?: (effect: EffectApi<any, any>) => EffectApi<any, any>
+    }
 
-  export type Opts<S, A> = Plugin<S, A> | Plugin<S, A>[]
+  export type Opts<State, Actions> = Plugin<State, Actions> | Plugin<State, Actions>[]
 
-  export interface Return<S, A> {
-    state: S
-    actions: A
-    subscribe: (fn: Subscriber<S, A>) => () => void
+  export interface Return<State, Actions> {
+    state: State
+    actions: Actions
+    subscribe: (callback: Subscriber<State, Actions>) => () => void
   }
   export type State = any
 
-  export type Computed<S> = (state: S) => S
+  export type Computed<State> = (state: State) => State
 
-  export type ReducerImpl<S, P = any> = (state: S, payload: P) => Partial<S>
-  export type ReducerApi<S, P = any> = (payload: P) => S
-  export interface Reducer<S, P = any> {
-    implementation: ReducerImpl<S, P>
-    api: ReducerApi<S, P>
+  export type ReducerImpl<State, Payload = any> = (state: State, payload: Payload) => Partial<State>
+  export type ReducerApi<State, Payload = any> = (payload: Payload) => State
+  export interface Reducer<State, Payload = any> {
+    implementation: ReducerImpl<State, Payload>
+    api: ReducerApi<State, Payload>
   }
 
-  export type Reducer0Impl<S, P = any> = (state: S) => Partial<S>
-  export type Reducer0Api<S> = () => S
-  export interface Reducer0<S> {
-    implementation: Reducer0Impl<S>
-    api: Reducer0Api<S>
+  export type Reducer0Impl<State, Payload = any> = (state: State) => Partial<State>
+  export type Reducer0Api<State> = () => State
+  export interface Reducer0<State> {
+    implementation: Reducer0Impl<State>
+    api: Reducer0Api<State>
   }
 
-  export type EffectImpl<S, A, P = any, R = void> = (state: S, actions: A, payload: P) => R
-  export type EffectApi<P = null, R = void> = (payload: P) => R
-  export interface Effect<S, A, P = any, R = void> {
-    implementation: EffectImpl<S, A, P, R>
-    api: EffectApi<P, R>
+  export type EffectImpl<State, Actions, Payload = any, Return = void> = (state: State, actions: Actions, payload: Payload) => Return
+  export type EffectApi<Payload = null, Return = void> = (payload: Payload) => Return
+  export interface Effect<State, Actions, Payload = any, Return = void> {
+    implementation: EffectImpl<State, Actions, Payload, Return>
+    api: EffectApi<Payload, Return>
   }
 
-  export type Effect0Impl<S, A, R = void> = (state: S, actions: A) => R
-  export type Effect0Api<R = void> = () => R
-  export interface Effect0<S, A, R = void> {
-    implementation: Effect0Impl<S, A, R>
-    api: Effect0Api<R>
+  export type Effect0Impl<State, Actions, Return = void> = (state: State, actions: Actions) => Return
+  export type Effect0Api<Return = void> = () => Return
+  export interface Effect0<State, Actions, Return = void> {
+    implementation: Effect0Impl<State, Actions, Return>
+    api: Effect0Api<Return>
   }
 
   // Quick way to create actions from reducers & effects
-  export type Actions<R, E> = ReducersApi<R> & EffectsApi<E>
+  export type Actions<Reducers, Effects> = ReducersApi<Reducers> & EffectsApi<Effects>
 
   // Pluck out reducer & effect implementationementations and apis (see types for reducers and effects below)
-  export type ReducersImpl<R extends any> = { [P in keyof R]: R[P]['implementation'] }
-  export type EffectsImpl<E extends any> = { [P in keyof E]: E[P]['implementation'] }
-  export type ReducersApi<R extends any> = { [P in keyof R]: R[P]['api'] }
-  export type EffectsApi<E extends any> = { [P in keyof E]: E[P]['api'] }
+  export type ReducersImpl<Reducers extends any> = {[Reducer in keyof Reducers]: Reducers[Reducer]['implementation']}
+  export type EffectsImpl<Effects extends any> = {[Effect in keyof Effects]: Effects[Effect]['implementation']}
+  export type ReducersApi<Reducers extends any> = {[Reducer in keyof Reducers]: Reducers[Reducer]['api']}
+  export type EffectsApi<Effects extends any> = {[Effect in keyof Effects]: Effects[Effect]['api']}
 
-  export interface Model<S, R, E> {
-    state: S
-    reducers?: ReducersImpl<R>
-    effects?: EffectsImpl<E>
+  export interface Model<State, Reducers, Effects> {
+    state: State
+    reducers?: ReducersImpl<Reducers>
+    effects?: EffectsImpl<Effects>
     scoped?: boolean
-    computed?: Computed<S>
+    computed?: Computed<State>
     models?: Record<string, Model<any, any, any>>
   }
-  export interface ModelApi<S, A> {
-    state: S
-    actions: A
+  export interface ModelApi<State, Actions> {
+    state: State
+    actions: Actions
   }
-  export type Models<M extends any> = {
-    state: { [N in keyof M]: M[N]['state'] }
-    actions: { [N in keyof M]: M[N]['actions'] }
+  export type Models<Models extends any> = {
+    state: {[Model in keyof Models]: Models[Model]['state']}
+    actions: {[Model in keyof Models]: Models[Model]['actions']}
   }
 }
 
